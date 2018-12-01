659a78813fecebe20362c06bb194ee2080c968dd
wuchp <1217631035@qq.com>
2018-12-01 22:25:14 +0800

    增加maven

---
+++ src/main/java/com/open/designpatterndemo/DesignPatternDemoApplication.java
@@ -0,0 +1,12 @@
+package com.open.designpatterndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternDemoApplication.class, args);
+	}
+}
